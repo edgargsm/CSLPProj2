762a1d6b2b5ae0f8dbbbd120ca035d033a2ab732
joaonpsilva <joaonuno.dragao@gmail.com>
2019-10-28 19:56:42 +0000

    resize water mark

--- src/waterMarker.c
+++ src/waterMarker.c
@@ -3,19 +3,41 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include "structs.h"
+#include <math.h>
 ///@file
 /**
-Beginning in (X,Y) pixel from SrcImage and (0,0) pixel from WaterMarkImg, will substitute each SrcImage Pixel for an average
-of the the corresponding pixel from both images.
+Beginning in (X,Y) pixel from SrcImage and (0,0) pixel from WaterMarkImg (following right and down directions), will substitute
+each SrcImage Pixel for an average of the the corresponding pixel from both images.
 
 If the pixel value from the WaterMarkImg is 0 0 0 (Black) the SrcImage pixel will keep his original value (Create water mark with
 no background colour)
+
+If the WaterMarkImg has not enought space the program will resize automatically.
+
 */
 void makeWaterMark(RGBIMAGE *im, RGBIMAGE *wtr, int x, int y){
+    int xleft = im->x - x;              ///First Calculate the remaining space SrcImage has.
+    int yleft = im->y - y;
 
-    for (int i = x, i2 = 0 ; i < im->x && i2 < wtr->x; i++, i2++){
-        for (int n = y, n2 = 0; n < im->y && n2 < wtr->y; n++, n2++){
-            
+    double xjump = (double) wtr->x / (double) xleft ;     ///Calculate how many pixels do we have to "jump" each time to fit WaterMarkImg
+    double yjump = (double) wtr->y / (double) yleft;
+
+    if (xjump <= 1){      ///If jump is less than 1 don do theese jumps, image fits.
+        xjump = 1;
+    }
+    if (yjump <= 1)
+        yjump = 1;
+    
+    double i2keepingfloat = 0;
+    double n2keepingfloat = 0;
+    for (int i = x, i2 = 0; i < im->x && i2 < wtr->x; i++,                                              ///Iterate over both images
+                                                        i2keepingfloat = i2keepingfloat + xjump,      ///keep iterator with jumps and another (int) to iterate over the image
+                                                        i2 = round(i2keepingfloat)){                    ///This is needed because if "jump" if ex: 1.4, the 0.4 will be forgot due to round. The image would not shrink at all.
+
+        for (int n = y, n2 = 0; n < im->y && n2 < wtr->y; n++,
+                                                            n2keepingfloat = n2keepingfloat + yjump,
+                                                            n2 = round(n2keepingfloat)){
+
             unsigned char r1 = im->rgb[i][n].pix[0];
             unsigned char g1 = im->rgb[i][n].pix[1];
             unsigned char b1 = im->rgb[i][n].pix[2];
@@ -27,7 +49,10 @@ void makeWaterMark(RGBIMAGE *im, RGBIMAGE *wtr, int x, int y){
                 for (int p = 0; p<3; p++)
                     im->rgb[i][n].pix[p] = (unsigned char) ((im->rgb[i][n].pix[p]+wtr->rgb[i2][n2].pix[p]) / 2);
             }
+
+            
         }
+        n2keepingfloat = 0;
     }
 
 }
